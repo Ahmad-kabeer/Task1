1d1e7df6ca27529e63d9828177e54bfd268341a8
AHMAD-KABEER <ahmad.kabeer244@gmail.com>
2021-02-19 21:52:00 +0530

    Basic Infrastructure.

---
+++ main.js
@@ -0,0 +1,7 @@
+<script>
+var d = new Date("October 13, 2014 11:13:00");
+document.getElementById("demo").innerHTML = d;
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+</script>
